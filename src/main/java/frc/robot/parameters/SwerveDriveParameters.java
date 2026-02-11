97ed402abc878739224a383afbc32ab5570d89bd
NRG948DriverStation <35410266+NRG948DriverStation@users.noreply.github.com>
2026-02-10 18:15:20 -0800

    Fixed swerve offsets on practice bot (#47)

--- src/main/java/frc/robot/parameters/SwerveDriveParameters.java
+++ src/main/java/frc/robot/parameters/SwerveDriveParameters.java
@@ -45,7 +45,7 @@ public enum SwerveDriveParameters {
       KrakenX44,
       new int[] {1, 2, 3, 4, 5, 6, 7, 8}, // drive, steer motor controller CAN IDs
       new int[] {31, 32, 33, 34}, // CANCoder CAN IDs
-      new double[] {-23.29, -57.74, 178.59, 77.70}, // CANCoder offsets
+      new double[] {157.76, 120.32, -1.67, -100.72}, // CANCoder offsets
       21),
   /**
    * Parameters for the 2026 Competition Base robot.
